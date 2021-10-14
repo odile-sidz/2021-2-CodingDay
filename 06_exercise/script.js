5c2765b2f69bd427eb477d6bfa91e3a3acfa413c
odile <odile.sidzikauskiene@powercoders.org>
2021-10-14 10:41:04 +0200

    ex6

---
+++ 06_exercise/script.js
@@ -0,0 +1,28 @@
+let weekNumbersToWords;
+weekNumbersToWords = prompt(`enter the number of the day`);
+
+switch (weekNumbersToWords) {
+case `1`:
+    console.log(`Monday`);
+    break;
+case `2`:
+    console.log(`Tuesday`);
+    break;   
+case `3`:
+    console.log(`Wednesday`);
+    break;
+case `4`:
+    console.log(`Thursday`);
+    break;
+case `5`:
+    console.log(`Friday`);
+    break;
+case `6`:
+    console.log(`Saturday`);
+    break;
+case `7`:
+    console.log(`Sunday`);
+    break;
+default :
+console.log(`this is not a number`);
+}
